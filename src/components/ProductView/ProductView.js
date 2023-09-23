a33b20c750132233e3ed3e52f0a807639cac4710
noah <noahboahk@gmail.com>
2023-09-23 15:22:09 -0700

    phase 3 and 4

--- src/components/ProductView/ProductView.js
+++ src/components/ProductView/ProductView.js
@@ -10,6 +10,23 @@ function ProductView({ products }) {
   const [sideOpen, setSideOpen] = useState(true);
   const [selectedProduct, setSelectedProduct] = useState(null);
 
+
+  // Open side panel when product is selected
+  useEffect(() => {
+    console.log(`selectedProduct CHANGED TO`, selectedProduct);
+    if (selectedProduct)
+      setSideOpen(true);
+  }, [selectedProduct]);
+
+  // Deselect product when side panel is closed
+  useEffect(() => {
+    console.log(`sideOpen CHANGED TO`, sideOpen);
+    if (!sideOpen)
+      setSelectedProduct();
+  }, [sideOpen]);
+
+
+
   return (
     <div className="product-view">
       <div className="product-main-area">
